2f546682ca1e48606d170ef78fdf4c2a144c6e26
Aline <alinecris@gmail.com>
2020-12-16 22:18:28 +0100

    Configure authentication

--- pages/Auth/Login/index.tsx
+++ pages/Auth/Login/index.tsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import MainComponent from '../../../components/shared/MainComponent'
 import LoginForm from '../../../components/LoginForm'
+import SignUpForm from '../../../components/SignUpForm'
 
 const Login: React.FC = () => {
   return (
     <MainComponent>
+      <br />
       <LoginForm titlePhrase="Log In" buttonPhrase="Log In" />
+      <br />
+      <SignUpForm titlePhrase="Create new account" buttonPhrase="CREATE" />
     </MainComponent>
   )
 }
--- services/api.ts
+++ services/api.ts
@@ -1,5 +1,5 @@
 import axios from 'axios'
-import Cookie from 'js-cookie'
+import Cookies from 'js-cookie'
 import ApiData from '../dtos/apiData'
 
 const api = axios.create({
