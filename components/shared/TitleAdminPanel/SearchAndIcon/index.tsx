f6d2883e797b544c39672e0fcf69e9efb0b12b4f
Aline <alinecris@gmail.com>
2021-01-19 20:50:33 +0100

    Add search

--- components/shared/TitleAdminPanel/SearchAndIcon/index.tsx
+++ components/shared/TitleAdminPanel/SearchAndIcon/index.tsx
@@ -1,35 +1,87 @@
 import React from 'react'
 import { Row, Col } from 'react-bootstrap'
+import { useState, useEffect } from 'react'
+import { IconProp } from '@fortawesome/fontawesome-svg-core'
+import styles from '../../../../styles/AdminTitle.module.css'
 import { InputGroup, FormControl, Row, Col } from 'react-bootstrap'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faSearch } from '@fortawesome/free-solid-svg-icons'
-import styles from '../../../../styles/AdminTitle.module.css'
 import StyledButton from '../../StyledButton'
-import { IconProp } from '@fortawesome/fontawesome-svg-core'
+import Link from 'next/link'
+
+import { useDispatch } from 'react-redux'
+import {
+  setSearch as setSearchRedux,
+  clearSearch
+} from '../../../../store/modules/admin/shared/search/reducer'
+
+import { useRouter } from 'next/router'
 
 interface SearchAndIcon {
-  icon: IconProp
+  icon: IconProp;
+  newPath: string;
 }
 
-const SearchAndIcon: React.FC<SearchAndIcon> = ({icon}) => {
+const SearchAndIcon: React.FC<SearchAndIcon> = ({icon, newPath}) => {
+  const [search, setSearch] = useState('')
+  const dispatch = useDispatch()
+  const router = useRouter()
+
+  useEffect(() => {
+    dispatch(clearSearch())
+  }, [])
+
+  const handleSearch = (): void => {
+    router.replace(router.pathname, '?page=1');
+    dispatch(setSearchRedux(search));
+  }
+
   return (
     <Row>
       <Col lg={9} xs>
         <Row>
           <Col lg={9} xs={10}>
             <InputGroup>
-              <FormControl placeholder="Pesquisar" className={styles.input} />
+              <FormControl
+                placeholder="Pesquisar usuário"
+                className={styles.input}
+                value={search}
+                onChange={
+                  (evt: React.ChangeEvent<HTMLInputElement>) => {
+                    setSearch(evt.target.value);
+                  }
+                }
+
+                onKeyPress={
+                  (evt: React.KeyboardEvent<HTMLInputElement>) => {
+                    if (evt.key.toLowerCase() === 'enter') {
+                      handleSearch()
+                    }
+                  }
+                }
+              />
             </InputGroup>
           </Col>
 
-          <Col lg={3} xs={2} className="mt-1" style={{cursor: 'pointer'}}>
-            <FontAwesomeIcon icon={faSearch} size="lg" color="var(--color-gray-light)" className="float-left" />
+          <Col lg={3} xs={2} className="mt-1"
+            style={{cursor: 'pointer'}}>
+            <FontAwesomeIcon
+              icon={faSearch}
+              size="lg"
+              color="var(--color-gray-light)"
+              className="float-left"
+              onClick={handleSearch}
+            />
           </Col>
         </Row>
       </Col>
 
-      <Col lg={2} xs={{span: 3}} className={styles.titleButton}>
-        <StyledButton icon={icon} type_button="blue" />
+      <Col lg={2} xs={{ span: 3 }} className={styles.titleButton}>
+        <Link href={newPath}>
+          <a>
+            <StyledButton icon={icon} type_button="blue" />
+          </a>
+        </Link>
       </Col>
     </Row>
   )
--- store/modules/rootReducer.ts
+++ store/modules/rootReducer.ts
@@ -2,6 +2,10 @@ import { combineReducers } from 'redux'
 
 import auth from './auth/reducer'
 
+import search from './admin/shared/search/reducer'
+
 export default combineReducers({
-  auth
+    auth,
+    category,
+    search
 })
